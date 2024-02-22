ba31953a33ff55b98b85b912f3caa5458cb66848
SpaceCrab <lundin.eric@hotmail.com>
2024-02-22 15:15:50 +0100

    added arduinotrundle.js
    started implementing functionallity. looking at cscsensor.js for
    guidance

---
+++ docs/ble/arduinotrundle.js
@@ -0,0 +1,62 @@
+import { BleDevice } from "./bledevice";
+
+/**
+ * Trundle wheel with arduino 
+ */
+
+export class ArduinoTrundleWheel extends BleDevice {
+    /**
+     * @typedef {Object} ATWMeasurement - arduino trundlewheel measurement object
+     * @property [number] cumulativeWheelRevolutions - number of revolutions of the trundlewheel
+     * 
+     */
+
+    /**
+     * @callback ATWcallback
+     * @param {ATWMeasurement} atw - arduino trundle wheel measurement
+     */
+
+    /**
+     * Creates a new sensor instance 
+     * @param {string} namePrefix
+     * @param {ATWcallback} atwcallback
+     */
+    constructor(namePrefix, atwcallback){
+        super()
+        this.atwcallback = atwcallback
+        this.namePrefix = namePrefix
+    }
+
+    /**
+     * Starts trundle wheel distance measurements
+     * @returns {PromiseLike}
+     */
+    async startNotificationsATWmeasurement(){
+        return this.startNotifications('atw_measurement', (event) => {
+            const value = event.target.value
+            const atw_measurement = this.parseATW(value)
+            this.atwcallback(atw_measurement)
+        })
+    }
+
+    /**
+     * stops the trundle wheel measurements 
+     * @returns {PromiseLike} 
+     */
+    async stopNotificationsATWmeasurments (){
+        return this.stopNotifications('atw_measurements')
+    }
+
+    /**
+     * parses the raw bytes 
+     * @param {DataView} value - bytes as received by the sensor
+     * @returns {ATWMeasurement}
+     */
+
+    parseATW (value){
+        /**
+         * TODO: find out how the trundle wheel data looks like and how to handle it 
+         * TODO: return the result 
+         */
+    }
+}
